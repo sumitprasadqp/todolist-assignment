c24a934010d1288108acb046fe8a08f87197dbf6
Sumit Prasad <sumit.prasad.questionpro@noreply.github.com>
2026-05-08 01:43:17 +0530

    msw initial setup

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,4 @@
+export const IS_TEST_ENV = import.meta.env.MODE === 'test'
+export const IS_DEV_ENV = import.meta.env.MODE === 'development'
+export const API_BASE_URL = ''
+export const IS_MOCK_ENV = import.meta.env.MODE === 'mock'
