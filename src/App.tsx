23e92003cf7f2eebc2e5613b44aa58a57d5f33da
Sumit Prasad <sumit.prasad.questionpro@noreply.github.com>
2026-05-08 02:17:11 +0530

    msw setup complete
    page was getting re-rendered again and again and msw was trying to connect to /todos which was not there in the application at all. It was trying to register at some other path due to caching or something. removing the site data and refreshing enabled the service worker.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
 import {WuPrimaryNavbar} from '@npm-questionpro/wick-ui-lib'
+import './App.css'
+// import {TodoListScreen} from './screens/TodoListScreen'
+import {API_BASE_URL} from './constants/appConstants'
+import type {IServerResponse} from './types/IServerResponse'
+import type {IUser} from './types/IUser'
+import {useEffect, useState} from 'react'
+
+const fetchUser = async (): Promise<IServerResponse<IUser>> => {
+  return fetch(`${API_BASE_URL}user`, {
+    method: 'GET',
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Content-Type': 'application/json',
+    },
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json() as Promise<IServerResponse<IUser>>
+  })
+}
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<IUser | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetchUser()
+      .then(res => {
+        setUser(res.data)
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+  if (!user) {
+    throw new Error('User not found')
+  }
+
   return (
     <>
-      {/* <TodoListScreen /> */}
-      <WuPrimaryNavbar
-        Links={[
-          <a key="home" href="#" className="active">
-            Home
-          </a>,
-          <a key="about" href="#">
-            About
-          </a>,
-          <a key="services" href="#">
-            Services
-          </a>,
-          <a key="contact" href="#">
-            Contact
-          </a>,
-        ]}
-      />
+      <div>
+        <WuPrimaryNavbar
+          Links={[
+            <a key="home" href="#" className="active">
+              Home
+            </a>,
+            <a key="about" href="#">
+              About
+            </a>,
+            <a key="services" href="#">
+              Services
+            </a>,
+            <a key="contact" href="#">
+              Contact
+            </a>,
+          ]}
+        />
+        {/* <TodoListScreen /> */}
+      </div>
+      <h1>{user?.email}</h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,38 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import './index.css'
 import {App} from './App.tsx'
+import {IS_MOCK_ENV} from './constants/appConstants.ts'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import '@npm-questionpro/wick-ui-icon/dist/wu-icon.css'
 import '@npm-questionpro/wick-ui-lib/dist/style.css'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking(): Promise<void> {
+  if (!IS_MOCK_ENV) {
+    return
+  }
+
+  const {mswDevServer} = await import('./msw/mswDevServer')
+  const serviceWorkerUrl = `${import.meta.env.BASE_URL}mockServiceWorker.js`
+  console.log(serviceWorkerUrl)
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  await mswDevServer.start({
+    serviceWorker: {
+      url: serviceWorkerUrl,
+      options: {
+        scope: '',
+      },
+    },
+  })
+}
+
+enableMocking()
+  .then(() => {
+    ReactDom.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  })
+  .catch(err => console.error(err))
