716a98532ec952a179109549bd180252a957b160
Sumit Prasad <sumit.prasad.questionpro@noreply.github.com>
2026-05-08 02:41:18 +0530

    react query initial setup

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,6 +1,24 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import React from 'react'
 
 export const AppProviders: React.FC<React.PropsWithChildren> = ({children}) => {
   // Create a client using ref to avoid re-creating it on every render
-  return <>{children}</>
+  const queryClientRef = React.useRef<QueryClient | null>(null)
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    })
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
